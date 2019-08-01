e753fb69799c7fb58e10617780d8e73f2d72cffa
Mark Goldstein <markgoldstein12@gmail.com>
2019-08-01 12:07:34 -0400

    first commit

---
+++ dbmodule.py
@@ -0,0 +1,147 @@
+import mysql.connector
+import json
+import datetime
+
+def dbconnection():
+
+	#localhost
+	# mydb = mysql.connector.connect(
+	# 		host="localhost",
+	# 		user="root",
+	# 		passwd="",
+	# 		database="dbtest"
+	# )
+
+
+	#webhosteddb
+	mydb = mysql.connector.connect(
+		host="35.239.141.59",
+		user="backendteam",
+		passwd="UZSDmp7J2J2ZYHw",
+	#test db
+		database="test_db"
+	#deploymentdb
+	#   database="cisc3140"
+	)
+
+	return mydb
+
+
+#no input, returns all active users
+#returns fields: userid, username, email, first, last, avatarUrl
+def allUsers():	
+
+	mydb = dbconnection()
+	#create db cursor
+	cursor = mydb.cursor(buffered=True)
+	#sql statement
+	sql = '''SELECT * FROM users where isActive = 1'''
+
+	try:
+		cursor.execute(sql)
+	except mysql.connector.Error as err:
+		return json.dumps({'error':str(err)})
+	#execute sql statement
+	cursor.execute(sql)
+	resultSet = cursor.fetchall()		#save sql result set
+	#convert columns and rows into json data
+	jsonData = [dict(zip([key[0] for key in cursor.description], row)) for row in resultSet]
+	#close database connection
+
+	cursor.close()
+	mydb.close()
+
+	#catch datetime datatype error for json
+	def myconverter(o):
+		if isinstance(o, datetime.datetime):
+			return o.__str__()
+	return json.dumps({'users':jsonData}, default = myconverter)
+
+
+#input: columnValue (string), valueValue (string or int) 
+#options and types:
+#columnValue: valueValue
+#userId: int
+#username: string
+#email: string
+#output: userid, username, email, first, last, avatarUrl
+def findUsers(columnValue, valueValue):
+
+	mydb = dbconnection()
+	cursor = mydb.cursor(buffered=True)
+
+	sql = f"SELECT * FROM users WHERE {columnValue} = '{valueValue}' and isActive = 1"
+	
+	try:
+		cursor.execute(sql)
+	except mysql.connector.Error as err:
+		return json.dumps({'error':str(err)})
+	
+	resultSet = cursor.fetchall()		#save sql result set
+	#convert columns and rows into json data
+	jsonData = [dict(zip([key[0] for key in cursor.description], row)) for row in resultSet]
+
+	#close database connection
+	cursor.close()
+	mydb.close()
+	#catch datetime datatype error for json
+	def myconverter(o):
+		if isinstance(o, datetime.datetime):
+			return o.__str__()
+
+	return json.dumps({'users':jsonData}, default = myconverter)
+
+
+#input: email (string), password (hashed string), username (string), first (String), last (string), avatarUrl (string)
+#email and username must be unique (use findUser)
+#password should be hashed
+#all fields are required!!
+def insertUser(email, password, username, first, last, avatarurl):
+
+	mydb = dbconnection()
+	cursor = mydb.cursor(buffered=True)
+
+	sql = f"INSERT INTO users (email, password, username, first, last, avatarurl) VALUES ('{email}','{password}','{username}','{first}','{last}','{avatarurl}')"
+	
+	try:
+		cursor.execute(sql)
+		mydb.commit()
+	except mysql.connector.Error as err:
+		return json.dumps({'error':str(err)})
+
+	#close database connection
+	cursor.close()
+	mydb.close()
+	
+	def myconverter(o):
+		if isinstance(o, datetime.datetime):
+			return o.__str__()
+
+	return findUsers('username', username)
+
+
+
+#input: username (string)
+def deleteUser(username):
+
+	mydb = dbconnection()
+	cursor = mydb.cursor(buffered=True)
+
+	sql = f"UPDATE users SET isActive = 0 WHERE username = '{username}'"
+	
+	try:
+		cursor.execute(sql)
+		mydb.commit()
+	except mysql.connector.Error as err:
+		return json.dumps({'error':str(err)})
+	
+	#close database connection
+	cursor.close()
+	mydb.close()
+		
+	def myconverter(o):
+		if isinstance(o, datetime.datetime):
+			return o.__str__()
+
+	return f"user {username} deleted"
+	
---
+++ main.py
@@ -0,0 +1,179 @@
+from flask import Flask, render_template, request, redirect, jsonify
+import requests
+from flask_cors import CORS
+import os
+import json
+from database import dbmodule
+from flask_restplus import Resource, Api
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+@api.route('/test/<user>')
+class Test(Resource):
+    def get(self, user):
+        # print(type(dbmodule.allUsers()))
+        # # res = test().json()
+        # return dbmodule.allUsers()
+        # res = test().json()
+        res = json.loads(dbmodule.findUsers('username', user))
+        return res
+
+# @app.route('/test/<user>', methods=['GET'])
+# def testsql(user):
+#     # print(type(dbmodule.allUsers()))
+#     # # res = test().json()
+#     # return dbmodule.allUsers()
+#     # res = test().json()
+#     res = json.loads(dbmodule.findUsers('username', user))
+#     return res
+
+
+'''
+ALPHA Back-end API with CRUD(Create, read, update, delete)
+for a user-based loggin website.
+'''
+
+'''
+!!Endpoint to login!!
+1)Get the user details from front end (either from form or json)
+    -Front end will do validation on length and ensure client-side error checking
+    -Check to see if the email is taken
+2)Send json with success response or error if error (implement try blocks?)
+'''
+@app.route('/login', methods=['POST'])
+def login():
+    res = request.form
+
+    # Grab the user and pw
+    user = res['username']
+    password = res['password']
+
+    # Get the db query into a python dict
+    db_result = json.loads(dbmodule.findUsers('username', user))
+
+    # Grab the first result of users that match
+    dbuser = db_result['users'][0]
+
+    # User Validation to DB goes here
+    if(dbuser):
+        # Now that we know the user exists, validate the password
+        if (dbuser['password'] == password):
+            # Send token to allow user to login
+            return jsonify({"msg": "Credentials Valid!"}), 200
+    else:
+        return jsonify({"error": "Credentials Not Valid!"})
+    return jsonify({"error": "Credentials Not Valid!"})
+
+
+'''
+!!Endpoint to CREATE a new user!! #CREATE (C)RUD
+1)Get the user details from front end (either from form or json)
+    -Front end will do validation on length and ensure client-side error checking
+    -Check to see if the email is taken
+2)Send json with success response or error if error (implement try blocks?) 
+'''
+@app.route('/signup', methods=['POST', 'GET'])
+def sign_up():
+    if(request.method == 'POST'):
+        # Grab the form
+        res = request.form
+        email = res['email']
+        username = res['username']
+        password = res['password']
+        first = res['first']
+        last = res['last']
+        avatarurl = res['avatarurl']
+    # Validate Email is not taken!
+    # Add user record to the db
+    added_user = json.loads(dbmodule.insertUser(
+        email, username, password,  first, last, avatarurl))
+
+    resdb = added_user['error'] or added_user['users'][0]
+    # is_err = added_user.keys()
+
+    # print(res)
+    return jsonify({"response": resdb})
+    # if added_user['users'][0]:
+    #     return jsonify({"msg": "Signed up"}), 201
+    # else:
+    #     return jsonify({"error": added_user['error']}), 409
+    # return jsonify({"msg": "Error"}), 401
+
+
+'''
+!!Endpoint to GET an existing user!!#READ from C(R)UD
+1)Get the user details from front end (either from form or json)
+    -Front end will do validation on length and ensure client-side error checking
+    -Check to see if the email is taken
+2)Send json with success response or error if error (implement try blocks?)
+
+'''
+@app.route('/user', methods=['GET'])
+def user():
+    res = request.get_json()  # Grab the response as a python dict from json sent
+    # User Validation to DB goes here
+    user_wanted = res['user']
+    print(type(dbmodule.findUsers("username", user_wanted)))
+    response = json.loads(dbmodule.findUsers("username", user_wanted))
+    print(response)
+    found = len(response["users"]) > 0
+    if found:
+        return jsonify(response), 200
+    else:
+        return jsonify({"error": "User Not Found!"}), 404
+    return jsonify({"error": "Credentials Not Valid!"})
+
+
+'''
+!!Endpoint to update a user's info!!
+1)Get the user ID
+    -Get things they want to change
+        -We'll assume one thing changes at a time!
+    -Update the database with the things they wanted to edit
+        -Front end will do validation?
+2)Send json with success response or error if error (implement try blocks?)
+'''
+@app.route('/update', methods=['PUT'])
+def update_user(user, field):
+        # Grab the form data(could also be a json, if we front end sends that instead)
+    res = request.form
+    thing_to_update = res[field]
+    if get_user(user) != '()':  # Currently, the get_user returns '()' if no user is found
+        # Call db to do the update on that user's data
+        return jsonify({"msg": "Update Success"}), 202
+    else:
+        # Signal DB Error
+        return jsonify({"error": "User Invalid"}), 409
+    return jsonify({"Error": "Bad request"}), 400
+
+
+'''
+!!Endpoint to delete a user!!
+1)Get the user ID
+    -Update the database with removing the delete
+2)Send json with success response or error if error (implement try blocks?)
+'''
+@app.route('/deleteuser', methods=['DELETE'])
+def delete_user(user):
+    # Grab the form data(could also be a json, if we front end sends that instead)
+    res = request.form  # Grab the request's form
+    user_to_del = user
+    if get_user(user) != '()':  # Currently, the get_user returns '()' if no user is found
+        # Call db to delete that user's data
+        return jsonify({"msg": "Delete Success"}), 202
+    else:
+        # Signal DB Error
+        return jsonify({"error": "User Invalid"}), 409
+    return jsonify({"Error": "Bad request"}), 400
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
